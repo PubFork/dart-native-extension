3b26848cb1c0a815deec9608a68a779f11f3e6cc
yushulx <lingxiao1002@gmail.com>
2017-01-13 18:19:07 +0800

    Clean up code

--- sample_extension.cc
+++ sample_extension.cc
@@ -132,48 +132,6 @@ Dart_Handle HandleError(Dart_Handle handle) {
   return handle;
 }
 
-
-void SystemRand(Dart_NativeArguments arguments) {
-  Dart_EnterScope();
-  Dart_Handle result = HandleError(Dart_NewInteger(rand()));
-  Dart_SetReturnValue(arguments, result);
-  Dart_ExitScope();
-}
-
-
-void SystemSrand(Dart_NativeArguments arguments) {
-  Dart_EnterScope();
-  bool success = false;
-  Dart_Handle seed_object = HandleError(Dart_GetNativeArgument(arguments, 0));
-  if (Dart_IsInteger(seed_object)) {
-    bool fits;
-    HandleError(Dart_IntegerFitsIntoInt64(seed_object, &fits));
-    if (fits) {
-      int64_t seed;
-      HandleError(Dart_IntegerToInt64(seed_object, &seed));
-      srand(static_cast<unsigned>(seed));
-      success = true;
-    }
-  }
-  Dart_SetReturnValue(arguments, HandleError(Dart_NewBoolean(success)));
-  Dart_ExitScope();
-}
-
-uint8_t* randomArray(int seed, int length) {
-  if (length <= 0 || length > 10000000) {
-    return NULL;
-  }
-  uint8_t* values = reinterpret_cast<uint8_t*>(malloc(length));
-  if (NULL == values) {
-    return NULL;
-  }
-  srand(seed);
-  for (int i = 0; i < length; ++i) {
-    values[i] = rand() % 256;
-  }
-  return values;
-}
-
 char **readBarcode(char *pszFileName, int *out_len) {
 	char **values = NULL;
 
@@ -246,43 +204,6 @@ char **readBarcode(char *pszFileName, int *out_len) {
 	return values;
 }
 
-
-void wrappedRandomArray(Dart_Port dest_port_id,
-                        Dart_CObject* message) {
-  Dart_Port reply_port_id = ILLEGAL_PORT;
-  if (message->type == Dart_CObject_kArray &&
-      3 == message->value.as_array.length) {
-    // Use .as_array and .as_int32 to access the data in the Dart_CObject.
-    Dart_CObject* param0 = message->value.as_array.values[0];
-    Dart_CObject* param1 = message->value.as_array.values[1];
-    Dart_CObject* param2 = message->value.as_array.values[2];
-    if (param0->type == Dart_CObject_kInt32 &&
-        param1->type == Dart_CObject_kInt32 &&
-        param2->type == Dart_CObject_kSendPort) {
-      int seed = param0->value.as_int32;
-      int length = param1->value.as_int32;
-      reply_port_id = param2->value.as_send_port.id;
-      uint8_t* values = randomArray(seed, length);
-
-      if (values != NULL) {
-        Dart_CObject result;
-        result.type = Dart_CObject_kTypedData;
-        result.value.as_typed_data.type = Dart_TypedData_kUint8;
-        result.value.as_typed_data.values = values;
-        result.value.as_typed_data.length = length;
-        Dart_PostCObject(reply_port_id, &result);
-        free(values);
-        // It is OK that result is destroyed when function exits.
-        // Dart_PostCObject has copied its data.
-        return;
-      }
-    }
-  }
-  Dart_CObject result;
-  result.type = Dart_CObject_kNull;
-  Dart_PostCObject(reply_port_id, &result);
-}
-
 void wrappedBarcodeReader(Dart_Port dest_port_id,
 	Dart_CObject* message) {
 	Dart_Port reply_port_id = ILLEGAL_PORT;
@@ -329,19 +250,6 @@ void wrappedBarcodeReader(Dart_Port dest_port_id,
 	Dart_PostCObject(reply_port_id, &result);
 }
 
-
-void randomArrayServicePort(Dart_NativeArguments arguments) {
-  Dart_EnterScope();
-  Dart_SetReturnValue(arguments, Dart_Null());
-  Dart_Port service_port =
-      Dart_NewNativePort("RandomArrayService", wrappedRandomArray, true);
-  if (service_port != ILLEGAL_PORT) {
-    Dart_Handle send_port = HandleError(Dart_NewSendPort(service_port));
-    Dart_SetReturnValue(arguments, send_port);
-  }
-  Dart_ExitScope();
-}
-
 void barcodeReaderServicePort(Dart_NativeArguments arguments) {
 	Dart_EnterScope();
 	Dart_SetReturnValue(arguments, Dart_Null());
@@ -362,17 +270,8 @@ struct FunctionLookup {
 
 
 FunctionLookup function_list[] = {
-    {"SystemRand", SystemRand},
-    {"SystemSrand", SystemSrand},
-    {"RandomArray_ServicePort", randomArrayServicePort},
 	{"BarcodeReader_ServicePort", barcodeReaderServicePort},
     {NULL, NULL}};
-
-
-FunctionLookup no_scope_function_list[] = {
-  {"NoScopeSystemRand", SystemRand},
-  {NULL, NULL}
-};
 
 Dart_NativeFunction ResolveName(Dart_Handle name,
                                 int argc,
@@ -400,14 +299,6 @@ Dart_NativeFunction ResolveName(Dart_Handle name,
   if (result != NULL) {
     Dart_ExitScope();
     return result;
-  }
-
-  for (int i=0; no_scope_function_list[i].name != NULL; ++i) {
-    if (strcmp(no_scope_function_list[i].name, cname) == 0) {
-      *auto_setup_scope = false;
-      result = no_scope_function_list[i].function;
-      break;
-    }
   }
 
   Dart_ExitScope();
